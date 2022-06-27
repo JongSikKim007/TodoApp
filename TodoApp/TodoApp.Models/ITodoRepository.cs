241b7b7b34daa89ac559e6530c7bd7ac6ce5e6ba
JonSikKim007 <kimjsik007@gmail.com>
2022-06-27 15:27:01 +0900

    임 메모리 데이터베이스 맛보기

---
+++ TodoApp/TodoApp.Models/ITodoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TodoApp.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(Todo Model);//입력
+        List<Todo> GetAll(); //출력
+    }
+
+
+}
