2686a199bb9ec220762d376290b1959e441a404f
JonSikKim007 <kimjsik007@gmail.com>
2022-06-30 10:16:08 +0900

    파일로 저장

---
+++ TodoApp.ConsoleAppFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using TodoApp.Models;
+
+namespace TodoApp.ConsoleAppFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITodoRepository _repository = new TodoRepositoryFile(@"C:\TodoApp\Todos.txt");
+            List<Todo> todos = new List<Todo>();
+            todos = _repository.GetAll();
+            //[1] 기본 데이터 출력
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id} - {t.Title}({t.IsDone})");
+            }
+            Console.WriteLine();
+
+            //[2] 데이터 입력
+            Todo todo = new Todo { Title = "DataBase 학습", IsDone = true };
+            _repository.Add(todo);
+            todos = _repository.GetAll(); //다시 로드
+            //[3] 추가 데이터 출력
+            foreach (var t in todos)
+            {
+                Console.WriteLine($"{t.Id} - {t.Title}({t.IsDone})");
+            }
+        }
+    }
+}
---
+++ TodoApp/TodoApp.Models/03_02_TodoRepositoryFile.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TodoApp.Models
+{
+    public class TodoRepositoryFile : ITodoRepository
+    {
+        private readonly string _filePath;
+        private static List<Todo> _todos = new List<Todo>();
+        /*public TodoRepositoryFile()
+        {
+            _todos = new List<Todo>
+            {
+                new Todo { Id = 1, Title = "ASP.NET Core 학습", IsDone = false }, // 이런 형식을 개체 리터럴이라고 함
+                new Todo { Id = 2, Title = "Blazer 학습", IsDone = false },
+                new Todo { Id = 3, Title = "C# 학습", IsDone = true }
+            };
+        }
+        */
+        public TodoRepositoryFile(string filePath = @"C:\TodoApp\Todos.txt") 
+        {
+            this._filePath = filePath;
+           string[] todos = File.ReadAllLines(filePath, Encoding.Default);
+            foreach (var t in todos)
+            {
+                string[] line = t.Split(',');
+                _todos.Add(new Todo { Id = Convert.ToInt32(line[0]), Title = line[1], IsDone = Convert.ToBoolean(line[2]) });
+            }
+        }
+
+       public void Add(Todo model)
+        {
+            model.Id = _todos.Max(t => t.Id) + 1;
+            _todos.Add(model);
+
+            //파일로 저장
+            string data = "";
+            foreach (var t in _todos)
+            {
+                data += $"{t.Id},{t.Title},{t.IsDone}{Environment.NewLine}"; //한 줄 씩 줄 바꿈 \r\n과 같음
+            }
+            using (StreamWriter  sw = new StreamWriter(_filePath))
+            {
+                sw.Write(data);
+                sw.Close();
+                sw.Dispose();
+            }
+
+        }
+
+        public List<Todo> GetAll()
+        {
+            return _todos.ToList();
+        }
+    }
+
+
+}
